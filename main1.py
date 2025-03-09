ff26252525a8cf9317d0fb8dee81087f5124cb17
Sergey Litvinov <slitvinov@gmail.com>
2025-03-09 14:06:11 -0400

    up

--- main1.py
+++ main1.py
@@ -1,25 +1,33 @@
 import torch
 import torch.distributed as dist
+import torch.distributed.elastic.multiprocessing.errors
 import datetime
 import platform
 
-dist.init_process_group(timeout=datetime.timedelta(seconds=10))
-local_rank = dist.get_node_local_rank()
-node = platform.node()
-rank = dist.get_rank()
-size = dist.get_world_size()
-torch.cuda.set_device(local_rank)
+@dist.elastic.multiprocessing.errors.record
+def main():
+    dist.init_process_group(timeout=datetime.timedelta(seconds=10))
+    local_rank = dist.get_node_local_rank()
+    node = platform.node()
+    rank = dist.get_rank()
+    size = dist.get_world_size()
+    torch.cuda.set_device(local_rank)
 
-if rank == 0:
-    objects = [42, [1, 2, 3, 4]]
+    if rank == 0:
+        x = torch.tensor(123, device="cuda")
+        objects = [42, [1, 2, 3, 4]]
+    else:
+        x = torch.empty([], device="cuda")
+        objects = [None, None]
     dist.broadcast_object_list(objects, 0)
-else:
-    objects = [None, None]
-    dist.broadcast_object_list(objects, 0)
+    dist.broadcast(x, 0)
 
-for i in range(size):
-    if rank == i:
-        print(rank, local_rank, node,
-              torch.cuda.get_device_properties().uuid, *objects)
-    dist.barrier()
-dist.destroy_process_group()
+    for i in range(size):
+        if rank == i:
+            print(rank, local_rank, node,
+                  torch.cuda.get_device_properties().uuid, *objects, x)
+        dist.barrier()
+    dist.destroy_process_group()
+
+if __name__ == '__main__':
+    main()
--- main2.sh
+++ main2.sh
@@ -3,7 +3,7 @@
 # salloc --gpus-per-node 2 -N 2 --mem 20Gb --no-shell
 # i=6053278
 # sh main2.sh --jobid $i
-master_addr=`srun "$@" sh -xeuc 'if test $SLURM_PROCID -eq 0; then hostname; fi'`
+master_addr=`srun "$@" sh -euc 'if test $SLURM_PROCID -eq 0; then hostname; fi'`
 srun "$@" sh -xeuc '
      torchrun \
        --master-addr '$master_addr' \
