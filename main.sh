3c34d5dfe70d4c6aa5ef97ecf55a91b02656e50a
Sergey Litvinov <slitvinov@gmail.com>
2025-03-09 02:45:24 +0100

    restore main.py

--- main.sh
+++ main.sh
@@ -1,2 +1,4 @@
-torchrun --nnodes 2 --node-rank 0 main.py --master-addr localhost &
-torchrun --nnodes 2 --node-rank 1 main.py --master-addr localhost &
+OMP_NUM_THREADS=1
+export OMP_NUM_THREADS
+torchrun --nnodes 2 --node-rank 0 --nproc-per-node cpu main.py &
+torchrun --nnodes 2 --node-rank 1 --nproc-per-node cpu main.py &
