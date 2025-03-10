e623fecddd16463b0f3463dc290e2b32a2efd683
Sergey Litvinov <slitvinov@gmail.com>
2025-03-10 15:27:47 +0100

    seas_gpu

--- main2.sh
+++ main2.sh
@@ -1,6 +1,6 @@
 # module load python
 # mamba activate env
-# salloc --gpus-per-node 2 -N 2 --mem 20Gb --no-shell
+# salloc -p seas_gpu --gpus-per-node 2 -N 2 --mem 20Gb --no-shell
 # i=6053278
 # sh main2.sh --jobid $i
 master_addr=`srun "$@" sh -euc 'if test $SLURM_PROCID -eq 0; then hostname; fi'`
