05886aad2d3678a183bb98f4c5f0428158195902
Sergey Litvinov <slitvinov@gmail.com>
2025-03-12 14:52:55 +0100

    up

--- main1.sh
+++ main1.sh
@@ -1,3 +1,3 @@
-mpiexec.openmpi -n 4 \
+TORCH_LOGS=+distributed,+dist_c10d PYTHONUNBUFFERED=1 mpiexec.openmpi -n 4 \
 		sh -xeuc '
-PYTHONUNBUFFERED=1 torchrun --nnodes $OMPI_COMM_WORLD_SIZE --node-rank $OMPI_COMM_WORLD_RANK main.py'
+torchrun --nnodes $OMPI_COMM_WORLD_SIZE --node-rank $OMPI_COMM_WORLD_RANK main.py'
